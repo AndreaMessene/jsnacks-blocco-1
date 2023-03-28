df015dd9d7843938cdbcc45c4d94ede9ca5939d4
AndreaMessene <amessene97@gmail.com>
2023-03-28 11:01:44 +0200

    esercizio 1

---
+++ step-1/main.js
@@ -0,0 +1,10 @@
+const numero1 = prompt ('Inserisci numero');
+
+const numero2 = prompt ('Inserisci numero');
+
+if (numero1 > numero2) {
+    console.log (`${numero1} vince giocatore 1`);
+}else if (numero1 < numero2) {
+    console.log (`${numero2} vince giocatore 2`);
+}
+
